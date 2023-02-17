21097f8efd74b4e7b6a93d0d258df4c963f0b95d
Rodrigo Azevedo Silva <rodrigo.azvd750@gmail.com>
2023-02-17 00:30:33 -0300

    navBar

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
 const menu = document.querySelector('.menu')
+const menuOptions = document.querySelector('.menu-options')
 const hambBtn = document.querySelector('.hamburguer')
+const container = document.querySelector('.container')
+let click = false
 
-hambBtn.addEventListener('click', (ev) => {
-    ev.preventDefault();
+hambBtn.addEventListener('click', () => {
+    click = true
 
-    if(menu.style.display == 'flex') {
+    if (menu.style.display == 'flex') {
         menu.style.display = 'none'
     } else {
         menu.style.display = 'flex'
+        menu.style.alignItems = 'center'
+        menuOptions.style.width = '70%'
     }
-
 })
